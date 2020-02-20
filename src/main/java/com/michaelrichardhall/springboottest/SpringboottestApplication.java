9592461b91d494b989103f0b46e3a7dfb2158dfc
Michael Hall <michaelrichardhall@gmail.com>
2020-02-20 18:17:27 +0000

    basic spring boot project

---
+++ src/main/java/com/michaelrichardhall/springboottest/SpringboottestApplication.java
@@ -0,0 +1,13 @@
+package com.michaelrichardhall.springboottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboottestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringboottestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michaelrichardhall/springboottest/SpringboottestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michaelrichardhall.springboottest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringboottestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
